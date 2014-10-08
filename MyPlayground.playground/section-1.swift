33d7cd1d3963369831a40b74f84f7bdd07766a67
Guilherme Oderdenge <guilhermeoderdenge@gmail.com>
2014-10-08 20:48:37 -0300

    First release

---
+++ MyPlayground.playground/section-1.swift
@@ -0,0 +1,11 @@
+let onSaleInferred = true
+
+let nameInferred = "Whoopie Cushion"
+
+let priceInferred = 4.99
+
+if onSaleInferred {
+    println("\(nameInferred) on sale for \(priceInferred)!");
+} else {
+    println("\(nameInferred) at regular price: \(priceInferred)!")
+}
